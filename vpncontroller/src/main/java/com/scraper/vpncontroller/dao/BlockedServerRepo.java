4ccb9c0b18f7cd5d5357054275992983b9a5b70a
Piyush Darshan <54273159+piyush302@users.noreply.github.com>
2023-05-25 13:03:19 +0530

    Delete vpncontroller directory

--- vpncontroller/src/main/java/com/scraper/vpncontroller/dao/BlockedServerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.scraper.vpncontroller.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.scraper.vpncontroller.model.BlockedServers;
-
-public interface BlockedServerRepo extends JpaRepository<BlockedServers, Integer>{
-
-}
